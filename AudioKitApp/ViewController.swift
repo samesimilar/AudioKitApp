a025125c0c9fc7a3cf8009afd85f613d7f4e3142
Mike Spears <mike.spears@utoronto.ca>
2018-02-01 17:18:43 -0500

    Starting point

--- AudioKitApp/ViewController.swift
+++ AudioKitApp/ViewController.swift
@@ -11,26 +11,20 @@ import AudioKitUI
 import AudioKit
 
 class ViewController: UIViewController, AKKeyboardDelegate, XYViewDelegate {
+
+    @IBOutlet weak var xyView: XYView!
     func updateValues(x: Float, y: Float) {
-        filter.cutoffFrequency = 20 + x * (22_050 - 20)
-        filter.resonance = -20 + (1 - y) * (40 - (-20))
-//        delay.time = 0.1 + (1 - y) * (1.0 - 0.1)
+
     }
     
-    @IBOutlet weak var xyView: XYView!
     @IBOutlet weak var containerView: UIView!
-    
-    var generator: AKOscillator!
-    var envelope: AKAmplitudeEnvelope!
-    var filter: AKLowPassFilter!
-    var delay: AKDelay!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         xyView.delegate = self
-        
-        
+       
         let frame = containerView.bounds
         let keyboardView = AKKeyboardView(width: Int(frame.width), height: Int(frame.height))
         keyboardView.octaveCount = 2
@@ -38,36 +32,15 @@ class ViewController: UIViewController, AKKeyboardDelegate, XYViewDelegate {
         keyboardView.delegate = self
         keyboardView.autoresizingMask = [UIViewAutoresizing.flexibleHeight, UIViewAutoresizing.flexibleWidth]
         containerView.addSubview(keyboardView)
-        
-        
-        let waveform = AKTable(.sawtooth)
-        generator = AKOscillator(waveform: waveform)
-        
-        filter = AKLowPassFilter(generator)
-        filter.cutoffFrequency = 220
-        
-        envelope = AKAmplitudeEnvelope(filter)
-        envelope.attackDuration = 0
-        
-        delay = AKDelay(envelope)
-        delay.time = 0.5
-        
-        let mixer = AKMixer(delay, envelope)
-        
-        AudioKit.output = mixer
-        AudioKit.start()
-        
-        generator.start()
+
     }
 
     func noteOn(note: MIDINoteNumber) {
-        let freq = note.midiNoteToFrequency()
-        generator.frequency = freq
-        envelope.start()
+
     }
     
     func noteOff(note: MIDINoteNumber) {
-        envelope.stop()
+        
     }
 
 }
